fb51d773b3428ef5a42ef140f30ad4fc01075aef
Jafet19 <kacur99@gmail.com>
2022-09-02 15:34:35 -0500

    commit tree f608b0b57b8a6415c2ccf1e7c2f01a3719df2f89

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+const LeastFavoriteCustomer = "Assignment to constant variable."
+var customerName = 'bob';
+ function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+ }
+ function setBestCustomer(){
+    bestCustomer = 'not bob'
+ }
+ function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+ }
+ function changeLeastFavoriteCustomer(){
+    LeastFavoriteCustomer = 'billy'
+ }
